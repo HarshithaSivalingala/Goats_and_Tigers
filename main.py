d36c61c021853ed82e9205bfb0fdcd3eaf81ccdd
pratyusha05 <pratyushap05@gmail.com>
2021-03-22 19:27:11 +0530

    added gameBoard method in the class gamePlay

---
+++ main.py
@@ -0,0 +1,21 @@
+import pygame
+pygame.init()
+
+
+class gamePlay:
+    def __init__(self, width, height):
+        self.width = width
+        self.height = height
+        screen = pygame.display.set_mode((width, height))
+        pygame.display.set_caption(Goats and Tiger)
+
+    def gameBoard(self):
+        running = True
+        while running:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    running = False
+
+
+game = gamePlay(1400, 800)
+game.gameBoard()
