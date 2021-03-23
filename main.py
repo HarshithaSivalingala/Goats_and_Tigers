2f85dd9da459837c675ca2eceb5b3dde786d27d1
pratyusha05 <pratyushap05@gmail.com>
2021-03-23 21:34:13 +0530

    added backgound

--- main.py
+++ main.py
@@ -10,20 +10,22 @@ class gamePlay:
         pygame.display.set_caption("Goats and Tiger")
 
     def gameBoard(self):
-        running = True
-        while running:
-            for event in pygame.event.get():
-                if event.type == pygame.QUIT:
-                    running = False
+            bg = pygame.image.load("resizedbg.jpg")
+            self.screen.blit(bg, (0, 0))
 
             color = (255, 255, 255)
             pygame.draw.polygon(self.screen, color, [[300,600],[700,600],[500,200]] , 2)
             pygame.display.flip()
             pygame.draw.lines(self.screen, color, True, [[300, 333.3], [700, 333.3],[700, 466.6],[300,466.6]], 2)
             pygame.draw.line(self.screen,color,(300,399.9),(700,399.9), 2)
-            pygame.draw.aalines(self.screen, color, False,[[433, 600], [500,200],[566, 600], [500, 200]],2)
-
+            pygame.draw.aalines(self.screen, color, False,[[433, 600], [500,200],[566, 600]],2)
 
 
 game = gamePlay(1400, 800)
-game.gameBoard()
+
+running = True
+while running:
+    game.gameBoard()
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
