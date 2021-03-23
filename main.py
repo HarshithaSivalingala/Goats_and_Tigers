92e29215f281aa1ebd60d035e319ed229b45f4a7
sritulasi <19wh1a0488@bvrithyderabad.edu.in>
2021-03-23 22:41:49 +0530

    Added the text on the board.

--- main.py
+++ main.py
@@ -10,21 +10,38 @@ class gamePlay:
         pygame.display.set_caption("Goats and Tiger")
 
     def gameBoard(self):
-            bg = pygame.image.load("notes and resources/brown_bg.jpg")
-            self.screen.blit(bg, (0, 0))
+        bg = pygame.image.load("notes and resources/brown_bg.jpg")
+        self.screen.blit(bg, (0, 0))
 
-            color = (255, 255, 255)
-            pygame.draw.polygon(self.screen, color, [[300, 600], [700, 600], [500, 200]], 2)
-            pygame.draw.lines(self.screen, color, True, [[300, 333.3], [700, 333.3], [700, 466.6], [300, 466.6]], 2)
-            pygame.draw.line(self.screen, color, (300, 399.9), (700, 399.9), 2)
-            pygame.draw.aalines(self.screen, color, False, [[433, 600], [500, 200], [566, 600]], 2)
-            pygame.display.flip()
+        color = (255, 255, 255)
+        pygame.draw.polygon(self.screen, color, [[300, 600], [700, 600], [500, 200]], 2)
+        pygame.draw.lines(self.screen, color, True, [[300, 333.3], [700, 333.3], [700, 466.6], [300, 466.6]], 2)
+        pygame.draw.line(self.screen, color, (300, 399.9), (700, 399.9), 2)
+        pygame.draw.aalines(self.screen, color, False, [[433, 600], [500, 200], [566, 600]], 2)
+        pygame.display.flip()
+
+    def scoreBoard(self):
+        BLACK = (0, 0, 0)
+        fontT = pygame.font.SysFont("monospace", 45)
+        text = fontT.render("Score Board", True, BLACK)
+        self.screen.blit(text, (800, 100))
+        font = pygame.font.SysFont("arial", 35)
+        text1 = font.render("Goats Left : ", True, BLACK)
+        self.screen.blit(text1, (750, 200))
+        text2 = font.render("Goats Captured : ", True, BLACK)
+        self.screen.blit(text2, (750, 300))
+        text3 = font.render("Tigers Cornered : ", True, BLACK)
+        self.screen.blit(text3, (750, 400))
+        pygame.display.flip()
+
 
 game = gamePlay(1200, 800)
 
 running = True
 while running:
     game.gameBoard()
+    game.scoreBoard()
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+    pygame.time.delay(30)
