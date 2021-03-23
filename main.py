2e248f2998b280d19bfcf9d5509d02043e8719a5
pratyusha05 <pratyushap05@gmail.com>
2021-03-23 22:10:31 +0530

    updated the background

--- main.py
+++ main.py
@@ -10,18 +10,17 @@ class gamePlay:
         pygame.display.set_caption("Goats and Tiger")
 
     def gameBoard(self):
-            bg = pygame.image.load("resizedbg.jpg")
+            bg = pygame.image.load("notes and resources/brown_bg.jpg")
             self.screen.blit(bg, (0, 0))
 
             color = (255, 255, 255)
-            pygame.draw.polygon(self.screen, color, [[300,600],[700,600],[500,200]] , 2)
+            pygame.draw.polygon(self.screen, color, [[300, 600], [700, 600], [500, 200]], 2)
+            pygame.draw.lines(self.screen, color, True, [[300, 333.3], [700, 333.3], [700, 466.6], [300, 466.6]], 2)
+            pygame.draw.line(self.screen, color, (300, 399.9), (700, 399.9), 2)
+            pygame.draw.aalines(self.screen, color, False, [[433, 600], [500, 200], [566, 600]], 2)
             pygame.display.flip()
-            pygame.draw.lines(self.screen, color, True, [[300, 333.3], [700, 333.3],[700, 466.6],[300,466.6]], 2)
-            pygame.draw.line(self.screen,color,(300,399.9),(700,399.9), 2)
-            pygame.draw.aalines(self.screen, color, False,[[433, 600], [500,200],[566, 600]],2)
 
-
-game = gamePlay(1400, 800)
+game = gamePlay(1200, 800)
 
 running = True
 while running:
