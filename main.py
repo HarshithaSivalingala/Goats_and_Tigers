88d13eb1a34633785411c7372acef9f32cd80454
sritulasi <19wh1a0488@bvrithyderabad.edu.in>
2021-03-24 10:49:10 +0530

    Made changes to stop images flicker.

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 import pygame
-import time
 pygame.init()
+clock = pygame.time.Clock()
 
 move = 0
 
-class gamePlay:
+
+class GamePlay:
     def __init__(self, width, height):
         self.width = width
         self.height = height
@@ -16,14 +17,15 @@ class gamePlay:
         self.screen.blit(bg, (0, 0))
 
         color = (255, 255, 255)
+        # creating A triangle
         pygame.draw.polygon(self.screen, color, [[300, 600], [700, 600], [500, 200]], 2)
         pygame.draw.lines(self.screen, color, True, [[300, 333.3], [700, 333.3], [700, 466.6], [300, 466.6]], 2)
         pygame.draw.line(self.screen, color, (300, 399.9), (700, 399.9), 2)
         pygame.draw.aalines(self.screen, color, False, [[433, 600], [500, 200], [566, 600]], 2)
-        pygame.display.flip()
 
     def scoreBoard(self):
         BLACK = (0, 0, 0)
+        # font for Title
         fontT = pygame.font.SysFont("monospace", 45)
         text = fontT.render("Score Board", True, BLACK)
         self.screen.blit(text, (800, 100))
@@ -34,16 +36,14 @@ class gamePlay:
         self.screen.blit(text2, (750, 300))
         text3 = font.render("Tigers Cornered : ", True, BLACK)
         self.screen.blit(text3, (750, 400))
-        pygame.display.flip()
 
     def drawTiger(self, tx, ty):
+        # Inserting tiger
         tiger = pygame.image.load('notes and resources/Tiger.png')
         tiger = pygame.transform.scale(tiger, (40, 40))
         rect = tiger.get_rect()
         rect.center = (tx, ty)
         self.screen.blit(tiger, rect)
-        pygame.display.flip()
-        pygame.time.delay(60)
         return tiger
 
     def movePiece(self, x, y):
@@ -58,7 +58,7 @@ class gamePlay:
         pass
 
 
-game = gamePlay(1200, 800)
+game = GamePlay(1200, 800)
 
 running = True
 while running:
@@ -75,4 +75,7 @@ while running:
             move += 1
             x, y = pygame.mouse.get_pos()
             game.movePiece(x, y)
-    pygame.time.delay(30)
+
+    pygame.display.flip()
+    pygame.display.update()
+    clock.tick(60)
